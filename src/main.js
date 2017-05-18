5bbdc8ad81463d7465c72ddcd7ba1640850461b9
Mike <mike.zhang@activenetwork.com>
2017-05-18 09:49:38 +0800

    add index page

--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
-
+import VueRouter from 'vue-router'
+import Layout from './components/layout'
+import IndexPage from './pages/index'
+Vue.use(VueRouter)
+let router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: IndexPage
+    }
+  ]
+})
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
+  template: '<Layout/>',
+  components: { Layout }
 })
---
+++ src/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    Index
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+
+        }
+    }
+}    
+</script>
+
+<style>
+
+</style>
